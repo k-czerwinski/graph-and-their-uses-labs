b817ee47f08fcb82a65b984975a7b82ed56f9a2c
Administrator <admin@example.com>
2024-03-10 10:33:28 +0100

    Created module which can generate random_graphs using edge existance probabilities or by specyfing number of edges

--- lab1/graph_drawing/utils.py
+++ lab1/graph_drawing/utils.py
@@ -7,8 +7,8 @@ def rename_nodes_labels(G:nx.Graph):
         G = nx.relabel_nodes(G, mapping)
         return G
 
-def draw_graph_on_circle_from_adjacency_matrix(adjacency_matrix:np.matrix):
+def draw_graph_on_circle_from_adjacency_matrix(adjacency_matrix:np.matrix, filename:str):
         G = nx.from_numpy_array(adjacency_matrix)
         G = rename_nodes_labels(G)
         nx.draw_circular(G, with_labels=True)
-        plt.savefig('./target/graph.png')
+        plt.savefig(filename)
