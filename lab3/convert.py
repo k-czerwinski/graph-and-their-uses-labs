bcddb90fb07c52f0cd61dd7dfcd3a8b377a9263c
unknown <wojtusmaciag@student.agh.edu.pl>
2024-04-14 20:37:15 +0100

    laby 3

---
+++ lab3/convert.py
@@ -0,0 +1,8 @@
+from graph_representation.SimpleGraph import  *
+
+file_path = "sample_data/adjacency_list.txt"
+adjacency_list = SimpleGraph.load_adjacency_list(file_path)
+graph = SimpleGraph(adjacency_list, GraphRepresentation.NEIGHBOURHOOD_LIST)
+
+np.savetxt('./target/adjacency_matrix.txt', graph.get_adjacency_matrix(), fmt='%i')
+np.savetxt('./target/incidence_matrix.txt', graph.get_incidence_matrix(), fmt='%i')
